7a725906686bf98473150f027d3e3b2c299c91a8
JMoli <JMoli@users.noreply.github.com>
2013-12-30 14:40:59 -0500

    Update settings.py

--- FoxyStructures/settings.py
+++ FoxyStructures/settings.py
@@ -83,6 +83,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static')
+STATIC_ROOT = ''
 
-STATIC_URL = 'FoxyStructures/static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'static').replace('\\', '/'),
+)
