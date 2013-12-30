65f9e3267e4e8eee2a64c448ef1148c6bbadbf30
JMoli <JMoli@users.noreply.github.com>
2013-12-30 14:18:53 -0500

    Update urls.py

--- FoxyStructures/urls.py
+++ FoxyStructures/urls.py
@@ -13,4 +13,11 @@ urlpatterns = patterns('',
     #url(r'^home$', home),
     url(r'^Pergola$', path),
     url(r'^Custom$', custom),
+    #url(r'^church$', church),
+    #url(r'^(pergola|gazebo|ramada)-kit/$', path),
+    #Pergolas
+    #url(r'^(?i)pergola-kit/(garden|backyard|oasis)/$', style),
+    #url(r'^(?i)pergola-kit/(garden|backyard|oasis)/(treated-pine|cedar|vinyl)$', material),
+    #url(r'^(?i)pergola-kit/(garden|backyard|oasis)/(treated-pine|cedar|vinyl)/(\d+)x(\d+)$', builder),
+    
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
